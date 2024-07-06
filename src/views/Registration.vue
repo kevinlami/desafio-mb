8a8f313ddac8a77733fbc337325b813990a7fab4
Kevin Lami <kevinlami15@gmail.com>
2024-07-06 09:39:01 -0300

    feat: create registration component

---
+++ src/views/Registration.vue
@@ -0,0 +1,18 @@
+<script setup>
+</script>
+
+<template>
+  <main>
+  </main>
+</template>
+
+<style scoped lang="scss">
+main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  max-width: $registration-width;
+  width: 100%;
+  margin: 100px auto 0;
+}
+</style>
